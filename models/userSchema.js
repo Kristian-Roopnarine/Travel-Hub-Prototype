328dc0cd6e71b45d542dd8d38a81a7b3c6aec92d
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-07 17:27:42 -0500

    (update) created save hooks to hash password and update passwordChangeAt before user is saved to database

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const cypto = require('crypto');
+const { nextTick } = require('process');
 
 const userSchema = mongoose.Schema({
   firstName: {
@@ -38,4 +39,18 @@ const userSchema = mongoose.Schema({
   },
   passwordResetToken: String,
   passwordResetExpires: Date,
+});
+
+userSchema.pre('save', function (next) {
+  if (!this.isMofidied('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isMofidied('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
