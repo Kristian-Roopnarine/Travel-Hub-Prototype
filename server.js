e6bfd8a0f7dd3b8fb0d71b52e02f39299507ddef
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-07 16:57:06 -0500

    (add) connected to hosted dev mongo database

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 process.on('uncaughtException', (err) => {
   console.log('Uncaught exception. Shutting down...');
   console.log(err.name, err.message);
 });
 
-const port = process.env.PORT || 5000;
+const port = config.app.port;
+
+mongoose
+  .connect(config.db.url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('Connection to database successful'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
