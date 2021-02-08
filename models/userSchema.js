de2d482c56859724fd113463581f04a3b3144aba
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-07 19:19:14 -0500

    (fix) incorrect method name

--- models/userSchema.js
+++ models/userSchema.js
@@ -41,13 +41,13 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.pre('save', function (next) {
-  if (!this.isMofidied('password') || this.isNew) return next();
+  if (!this.isModified('password') || this.isNew) return next();
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
 
 userSchema.pre('save', async function (next) {
-  if (!this.isMofidied('password')) return next();
+  if (!this.isModified('password')) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
