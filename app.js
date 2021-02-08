89ad6694eabd3fcdf8eb02ffbf042edec983d0a8
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-07 19:18:33 -0500

    (add) authentication router to express application

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 const morgan = require('morgan');
+
+const authRouter = require('./routes/authRoutes');
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -12,5 +15,15 @@ app.get('/', (req, res, next) => {
     message: 'App running',
   });
 });
+app.use('/api/v1/auth', authRouter);
+app.all('*', (req, res, next) => {
+  next(
+    new AppError(
+      `Could not find the requested URL ${req.protocol}://${req.hostname}${req.url}`,
+      404
+    )
+  );
+});
 
+app.use(globalErrorHandler);
 module.exports = app;
