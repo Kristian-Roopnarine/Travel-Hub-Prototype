fcbaa88d65b51453e30a9a675dfb8dded7357891
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-13 17:21:35 -0500

    (add) controllers to add members and retreive specific itineraries

--- controllers/itineraryController.js
+++ controllers/itineraryController.js
@@ -7,11 +7,41 @@ const config = require('./../config');
 // create itinerary
 exports.createItinerary = asyncCatchWrapper(async (req, res, next) => {
   const { _id } = req.user;
-  console.log({ _id });
   const { body } = req;
   const itinerary = await Itineraries.create({ creator: _id, ...body });
   res.status(201).json({
     status: 'created',
     data: itinerary,
   });
+});
+
+exports.getItinerary = asyncCatchWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const itinerary = await Itineraries.findById(id);
+  res.status(200).json({
+    data: itinerary,
+  });
+});
+
+exports.addMember = asyncCatchWrapper(async (req, res, next) => {
+  // first get owner id
+  const { id } = req.params;
+  const { members } = req.body;
+  // this is the itinerary
+  const itinerary = await Itineraries.findByIdAndUpdate(
+    id,
+    { $push: { members: members } },
+    { upsert: true, new: true, save: true }
+  );
+  const {
+    message,
+    statusCode,
+  } = appMessages.itinerary.update.success.addMembers;
+
+  res.status(statusCode).json({
+    status: 'success',
+    message,
+    data: itinerary,
+  });
+  console.log(res.body);
 });
