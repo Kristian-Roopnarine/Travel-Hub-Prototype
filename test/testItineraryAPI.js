a97cdf93d98723c603ab7ce865d2834fdb6401ce
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-13 17:23:41 -0500

    (add) unit tests for adding members, creating itineraries and retreiving itineraries

--- test/testItineraryAPI.js
+++ test/testItineraryAPI.js
@@ -1,39 +1,85 @@
 // make sure to set NODE_ENV=test
+//
 const config = require('../config');
 const request = require('supertest');
 const chai = require('chai');
-const mongoose = require('mongoose');
+const expect = chai.expect;
 const Users = require('./../models/userSchema');
 const Itineraries = require('./../models/itinerarySchema');
 const app = require('./../app');
 const appMessages = require('./../applicationMessages.json');
+const helpFunc = require('./_helperFunctions');
 const itineraryApi = '/api/v1/itinerary';
-async function addTestUser() {
-  return await Users.create({
-    firstName: 'bob',
-    lastName: 'bob',
-    email: 'bob@gmail.com',
-    password: '123',
-    passwordConfirm: '123',
-  });
-}
+
 var token = null;
 describe('#ItineraryAPI', function () {
   beforeEach(async function () {
     await Users.deleteMany({});
-    await addTestUser();
-    const loginInfo = { email: 'bob@gmail.com', password: '123' };
-    const response = await request(app)
-      .post('/api/v1/auth/login')
-      .send(loginInfo);
+    await helpFunc.addTestUser();
+    await helpFunc.addTestUser('bob2@gmail.com', '123');
+    await helpFunc.addTestItinerary('bob@gmail.com');
+    const response = await helpFunc.getJWT();
     token = response.body.token;
   });
-
-  it('POST / should return 201 on succesful itinerary creation', function (done) {
-    request(app)
+  /*
+  it('GET / should return all itineraries a user is part of', async function(){
+  })
+*/
+  it('POST / should return 201 on succesful itinerary creation', async function () {
+    const response = await request(app)
       .post(`${itineraryApi}/`)
       .set('Authorization', 'Bearer ' + token)
-      .send({ title: 'This is a test' })
-      .expect(201, done);
+      .send({ title: 'This is a test' });
+    const { body } = response;
+    expect(body.title).to.equal('This is a test');
+    expect(response.status).to.equal(201);
+  });
+
+  it('GET /:id should return 200 with the itinerary with specified id', async function () {
+    const itinerary = await Itineraries.findOne({
+      title: 'This is a test title',
+    }).exec();
+    const response = await request(app).get(`${itineraryApi}/${itinerary._id}`);
+    const { body } = response;
+    expect(response.status).to.equal(200);
+    expect(body.title).to.equal('This is a test title');
+  });
+
+  it('POST /:id/members should return 204 on successfully adding a new member to itinerary', async function () {
+    const member = await Users.findOne({ email: 'bob2@gmail.com' }).exec();
+    const itinerary = await Itineraries.findOne({
+      title: 'This is a test title',
+    }).exec();
+    const {
+      message,
+      statusCode,
+    } = appMessages.itinerary.update.success.addMembers;
+    var url = `${itineraryApi}/${itinerary._id}/members`;
+    const response = await request(app)
+      .post(url)
+      .set('Authorization', 'Bearer ' + token)
+      .send({ members: [member._id] });
+    const { body } = response;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
+    expect(body.data.members.length).to.equal(1);
+  });
+
+  it('POST /:id/members should return 401 when unauthorized user tries to add members to itinerary', async function () {
+    const response = await helpFunc.getJWT('bob2@gmail.com', '123');
+    token = response.body.token;
+    // find the itinerary id to send to the backend
+    const itinerary = await Itineraries.findOne({
+      title: 'This is a test title',
+    }).exec();
+    var url = `${itineraryApi}/${itinerary._id}/members`;
+    const {
+      message,
+      statusCode,
+    } = appMessages.itinerary.permissions.notAuthorized;
+    await request(app)
+      .post(url)
+      .set('Authorization', 'Bearer ' + token)
+      .expect(statusCode, { status: 'fail', message: message });
   });
 });
