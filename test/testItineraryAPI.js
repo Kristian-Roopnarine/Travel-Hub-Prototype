c2ca8e1534cfff189b222b43db1bfbf51b91e135
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-14 00:02:45 -0500

    (add) unit tests for all routes when itinerary does not exist and when member does not exist respective routes

--- test/testItineraryAPI.js
+++ test/testItineraryAPI.js
@@ -9,6 +9,7 @@ const Itineraries = require('./../models/itinerarySchema');
 const app = require('./../app');
 const appMessages = require('./../applicationMessages.json');
 const helpFunc = require('./_helperFunctions');
+const mongoose = require('mongoose');
 
 const itineraryApi = '/api/v1/itinerary';
 
@@ -40,10 +41,23 @@ describe('#ItineraryAPI', function () {
     const itinerary = await Itineraries.findOne({
       title: 'This is a test title',
     }).exec();
-    const response = await request(app).get(`${itineraryApi}/${itinerary._id}`);
+    const response = await request(app)
+      .get(`${itineraryApi}/${itinerary._id}`)
+      .set('Authorization', 'Bearer ' + token);
     const { data } = response.body;
     expect(response.status).to.equal(200);
     expect(data.title).to.equal('This is a test title');
+  });
+
+  it('GET /:id should return 404 when itinerary does not exist', async function () {
+    const id = mongoose.Types.ObjectId('zzzzzzzzzzzz');
+    const response = await request(app)
+      .get(`${itineraryApi}/${id}`)
+      .set('Authorization', 'Bearer ' + token);
+    const { body } = response;
+    const { message, statusCode } = appMessages.itinerary.doesNotExist;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
   });
 
   it('DELETE /:id should return 200 after deleting itinerary', async function () {
@@ -54,6 +68,40 @@ describe('#ItineraryAPI', function () {
       .delete(`${itineraryApi}/${itinerary._id}`)
       .set('Authorization', 'Bearer ' + token)
       .expect(204);
+  });
+
+  it('DELETE /:id should return 404 when itinerary does not exist', async function () {
+    const id = mongoose.Types.ObjectId('zzzzzzzzzzzz');
+    const response = await request(app)
+      .delete(`${itineraryApi}/${id}`)
+      .set('Authorization', 'Bearer ' + token);
+    const { body } = response;
+    const { message, statusCode } = appMessages.itinerary.doesNotExist;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
+  });
+
+  it('GET /:id/members returns all members for a specific itinerary', async function () {
+    const itinerary = await Itineraries.findOne({
+      title: 'This is a test title',
+    }).exec();
+    var url = `${itineraryApi}/${itinerary._id}/members`;
+    const response = await request(app)
+      .get(url)
+      .set('Authorization', 'Bearer ' + token);
+    const { data } = response.body;
+    expect(data).to.be.an('array');
+    expect(response.status).to.equal(200);
+  });
+  it('GET /:id/members should return 404 when itinerary does not exist', async function () {
+    const id = mongoose.Types.ObjectId('zzzzzzzzzzzz');
+    const response = await request(app)
+      .get(`${itineraryApi}/${id}/members`)
+      .set('Authorization', 'Bearer ' + token);
+    const { body } = response;
+    const { message, statusCode } = appMessages.itinerary.doesNotExist;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
   });
 
   it('POST /:id/members should return 2022 on successfully adding a new member to itinerary', async function () {
@@ -76,6 +124,17 @@ describe('#ItineraryAPI', function () {
     expect(data.members.length).to.equal(1);
   });
 
+  it('POST /:id/members should return 404 when itinerary does not exist', async function () {
+    const id = mongoose.Types.ObjectId('zzzzzzzzzzzz');
+    const response = await request(app)
+      .post(`${itineraryApi}/${id}/members`)
+      .set('Authorization', 'Bearer ' + token);
+    const { body } = response;
+    const { message, statusCode } = appMessages.itinerary.doesNotExist;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
+  });
+
   it('POST /:id/members should return 401 when unauthorized user tries to add members to itinerary', async function () {
     const response = await helpFunc.getJWT('bob2@gmail.com', '123');
     token = response.body.token;
@@ -92,5 +151,41 @@ describe('#ItineraryAPI', function () {
       .post(url)
       .set('Authorization', 'Bearer ' + token)
       .expect(statusCode, { status: 'fail', message: message });
+  });
+
+  it('DELETE /:id/members/:memId should return 204 when owner successfully deletes a member from the itinerary', async function () {
+    const member = await Users.findOne({ email: 'bob2@gmail.com' }).exec();
+    const itinerary = await Itineraries.findOne({
+      title: 'This is a test title',
+    }).exec();
+    var url = `${itineraryApi}/${itinerary._id}/members/${member._id}`;
+    await request(app)
+      .delete(url)
+      .set('Authorization', 'Bearer ' + token)
+      .expect(204);
+  });
+  it('DELETE /:id/members/:memId should return 404 when itinerary does not exist', async function () {
+    const id = mongoose.Types.ObjectId('zzzzzzzzzzzz');
+    const member = Users.findOne({ email: 'bob2@gmail.com' }).exec();
+    const response = await request(app)
+      .delete(`${itineraryApi}/${id}/members/${member._id}`)
+      .set('Authorization', 'Bearer ' + token);
+    const { body } = response;
+    const { message, statusCode } = appMessages.itinerary.doesNotExist;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
+  });
+  it('DELETE /:id/members/:memId should return 404 when member does not exist', async function () {
+    const itinerary = await Itineraries.findOne({
+      title: 'This is a test title',
+    }).exec();
+    const id = mongoose.Types.ObjectId('zzzzzzzzzzzz');
+    const response = await request(app)
+      .delete(`${itineraryApi}/${itinerary._id}/members/${id}`)
+      .set('Authorization', 'Bearer ' + token);
+    const { body } = response;
+    const { message, statusCode } = appMessages.users.doesNotExist;
+    expect(response.status).to.equal(statusCode);
+    expect(body.message).to.equal(message);
   });
 });
