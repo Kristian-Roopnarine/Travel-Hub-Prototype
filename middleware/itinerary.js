1e378bf0a950c4f4b2878fef158da4c9f6ab879d
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-14 00:01:04 -0500

    (add) function to add userid to request object and added condition to isOwner to check whether itinerary exists

--- middleware/itinerary.js
+++ middleware/itinerary.js
@@ -7,16 +7,23 @@ const appMessages = require('./../applicationMessages.json');
 exports.isOwner = asyncCatchWrapper(async (req, res, next) => {
   const { user } = req;
   const { id } = req.params;
-  const itinerary = await Itineraries.findOne({
-    creator: user._id,
-    _id: id,
-  }).exec();
+  const itinerary = await Itineraries.findById(id).exec();
   if (!itinerary) {
+    const { message, statusCode } = appMessages.itinerary.doesNotExist;
+    return next(new AppError(message, statusCode));
+  }
+  if (!user._id.equals(itinerary.creator)) {
     const {
       message,
       statusCode,
     } = appMessages.itinerary.permissions.notAuthorized;
     return next(new AppError(message, statusCode));
   }
+  next();
+});
+
+exports.setUseridAsCreator = asyncCatchWrapper(async (req, res, next) => {
+  const { user } = req;
+  req.body.creator = user._id;
   next();
 });
