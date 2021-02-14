365a0acd0b940bd3025d61473843440c02a1ece5
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-14 00:01:49 -0500

    (update) refactored itinerary routes by combining similar routes

--- routes/itineraryRoutes.js
+++ routes/itineraryRoutes.js
@@ -3,21 +3,39 @@ const router = express.Router();
 const itineraryController = require('./../controllers/itineraryController');
 const authController = require('./../controllers/authController');
 const itineraryMiddleware = require('./../middleware/itinerary');
+const userMiddleware = require('./../middleware/user');
+router
+  .route('/')
+  .post(
+    authController.protect,
+    itineraryMiddleware.setUseridAsCreator,
+    itineraryController.createItinerary
+  );
 
-router.get('/:id', itineraryController.getItinerary);
-router.delete(
-  '/:id',
-  authController.protect,
-  itineraryMiddleware.isOwner,
-  itineraryController.deleteItinerary
-);
-router.post('/', authController.protect, itineraryController.createItinerary);
+router
+  .route('/:id')
+  .get(authController.protect, itineraryController.getItinerary)
+  .delete(
+    authController.protect,
+    itineraryMiddleware.isOwner,
+    itineraryController.deleteItinerary
+  );
+router
+  .route('/:id/members')
+  .get(authController.protect, itineraryController.getAllMembers)
+  .post(
+    authController.protect,
+    itineraryMiddleware.isOwner,
+    itineraryController.addMember
+  );
 
-router.post(
-  '/:id/members',
-  authController.protect,
-  itineraryMiddleware.isOwner,
-  itineraryController.addMember
-);
+router
+  .route('/:id/members/:memId')
+  .delete(
+    authController.protect,
+    itineraryMiddleware.isOwner,
+    userMiddleware.userExists,
+    itineraryController.deleteMember
+  );
 
 module.exports = router;
