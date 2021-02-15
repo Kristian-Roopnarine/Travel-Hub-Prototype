15fddc61c4256a9112c689d4da9deb447e0d1917
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-15 16:03:51 -0500

    (add) boiler plate for Restaurant Api testing

---
+++ server/test/testRestaurantApi.js
@@ -0,0 +1,6 @@
+const Restaurants = require('./../models/restaurantSchema');
+const app = require('./../app');
+const permissionMessages = require('./../appMessages/permissions.json');
+const authMessages = require('./../appMessages/authentication.json');
+const helpFunc = require('./_helperFunctions');
+const mongoose = require('mongoose');
