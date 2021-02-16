316c1645b336bb3ac54d51713fdaa01a229ac409
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-15 19:32:20 -0500

    (add) router for restaurant api

---
+++ server/routes/restaurantRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('./../controllers/authController');
+const restaurantController = require('./../controllers/restaurantController');
+const restaurantMiddleware = require('./../middleware/restaurant');
+router
+  .route('/')
+  .post(
+    authController.protect,
+    restaurantMiddleware.setUseridAsAdvocate,
+    restaurantController.createRestaurant
+  );
+
+router
+  .route('/:id')
+  .get(authController.protect, restaurantController.getRestaurant)
+  .delete(authController.protect, restaurantController.deleleteRestaurant);
+
+module.exports = router;
