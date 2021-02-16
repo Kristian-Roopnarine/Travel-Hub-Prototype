6c3cbc7a46ab61089a0448b3d70e16b3a85af213
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-15 22:50:34 -0500

    (add) controller for /:id PATCH route

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,7 +1,25 @@
 const asyncCatchWrapper = require('./../utils/asyncCatchWrapper');
 const factory = require('./handlerFactory');
 const Restaurants = require('./../models/restaurantSchema');
+const { filterObj } = require('./../utils/filterObj');
 
 exports.createRestaurant = factory.createOne(Restaurants);
 exports.deleleteRestaurant = factory.deleteOne(Restaurants);
 exports.getRestaurant = factory.getOne(Restaurants);
+
+exports.updateRestaurant = asyncCatchWrapper(async (req, res, next) => {
+  const filteredBody = filterObj(req.body, 'advocate');
+  const { id } = req.params;
+  const updatedRestaurant = await Restaurants.findByIdAndUpdate(
+    id,
+    filteredBody,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).json({
+    staus: 'success',
+    data: updatedRestaurant,
+  });
+});
