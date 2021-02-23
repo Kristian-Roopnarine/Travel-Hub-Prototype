a11e800277342c906d6102e98fa6ab58ec5924da
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-02-23 18:34:23 -0500

    (add) GET / controller to return all restaurants when no itinerary id is provided, and itinerary specific restaurants if itinId in params

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -6,6 +6,18 @@ const { filterObj } = require('./../utils/filterObj');
 exports.createRestaurant = factory.createOne(Restaurants);
 exports.deleleteRestaurant = factory.deleteOne(Restaurants);
 exports.getRestaurant = factory.getOne(Restaurants);
+// TEST THIS
+exports.getAllRestaraunts = asyncCatchWrapper(async (req, res, next) => {
+  let filter = {};
+  if (req.params.itinId) filter = { itinerary: req.params.itinId };
+  const restaurants = await Restaurants.find(filter).exec();
+
+  res.status(200).json({
+    status: 'success',
+    results: restaurants.length,
+    data: restaurants,
+  });
+});
 
 exports.updateRestaurant = asyncCatchWrapper(async (req, res, next) => {
   const filteredBody = filterObj(req.body, 'advocate');
