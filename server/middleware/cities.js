c9480b179048d04f906417120c2ddb0120c46b20
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-01 17:14:02 -0500

    (add) middleware to check if city exists before creating itinerary

--- server/middleware/cities.js
+++ server/middleware/cities.js
@@ -1,4 +1,12 @@
 const Cities = require('./../models/citySchema');
 const asyncCatchWrapper = require('./../utils/asyncCatchWrapper');
+const AppError = require('./../utils/appError');
 
-exports.cityExists = asyncCatchWrapper(async (req, res, next) => {});
+exports.cityExists = asyncCatchWrapper(async (req, res, next) => {
+  const { city } = req.body;
+  const cityExists = await Cities.findById(city).exec();
+  if (!cityExists) {
+    return next(new AppError('Error finding city.', 404));
+  }
+  next();
+});
