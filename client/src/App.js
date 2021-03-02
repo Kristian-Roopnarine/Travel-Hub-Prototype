1aae5a0014b7c9cd41b0e72a3d2f883b4efd63b6
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-02 02:42:56 -0500

    (add) views and URLs for pages in react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import GoogleMaps from './components/GoogleMaps';
-import Login from './components/Login';
+import Home from './pages/Home';
+import Itinerary from './pages/Itinerary';
+import Profile from './pages/Profile';
+import TravelGuides from './pages/TravelGuides';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/map">
-          <GoogleMaps />;
-        </Route>
-        <Route path="/google-login">
-          <Login />
-        </Route>
+        <Route path="/" exact component={Home} />
+        <Route path="/itinerary" component={Itinerary} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/travel-guides" component={TravelGuides} />
       </Switch>
     </Router>
   );
