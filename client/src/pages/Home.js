07fc7ca3471fcc88392e30a51789526e0d4a9f43
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-02 02:43:11 -0500

    (add) starter code for views

---
+++ client/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <p className="font-mono">Home page</p>;
+}
+
+export default Home;
---
+++ client/src/pages/Itinerary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleMaps from './../components/GoogleMaps';
+
+function Itinerary() {
+  return (
+    <>
+      <p className="font-mono">Your itinerary </p>
+      <GoogleMaps />
+    </>
+  );
+}
+
+export default Itinerary;
