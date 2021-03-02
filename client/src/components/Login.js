c07e63ebe41f76ef66a6b89d8ffe62eaad6ade89
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-02 14:35:07 -0500

    (update) styling to create google login button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,36 +1,26 @@
 import React from 'react';
-import { GoogleLogin } from 'react-google-login';
-import axios from 'axios';
-const { REACT_APP_GOOGLE_CLIENT_ID } = process.env;
-
+import { FcGoogle } from 'react-icons/fc';
 function Login() {
   const onSuccess = (res) => {
     const { id_token } = res.getAuthResponse();
     console.log(id_token);
-    // send token id to backend
-    //
-    // redirect if successful
     console.log(res);
   };
 
   const onFailure = (res) => {
     console.log(res);
   };
-  /*
   return (
-    <div>
-      <GoogleLogin
-        clientId={REACT_APP_GOOGLE_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-        style={{ marginTop: '2rem' }}
-      />
+    <div className="flex items-center space-x-2 p-2 bg-white rounded-sm">
+      <FcGoogle size={'1.5rem'} />
+      <a
+        className="text-gray-500"
+        href="http://localhost:5000/api/v1/auth/google"
+      >
+        Sign in with Google
+      </a>
     </div>
- );
- */
-  return <a href="http://localhost:5000/api/v1/auth/google">Google Login</a>;
+  );
 }
 
 export default Login;
