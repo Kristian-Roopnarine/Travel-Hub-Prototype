51f0fdb564e6a7cf934cf3cfdf533baa19a0a7b5
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-02 12:18:01 -0500

    (update) testing url parameters in itinerary

--- client/src/pages/Itinerary.js
+++ client/src/pages/Itinerary.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import GoogleMaps from './../components/GoogleMaps';
+import { useParams } from 'react-router-dom';
 
 function Itinerary() {
+  let { id } = useParams();
   return (
     <>
+      <h2 className="font-mono">The ID of this itinerary is {id}</h2>
       <p className="font-mono">Your itinerary </p>
       <GoogleMaps />
     </>
