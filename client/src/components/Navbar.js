637df472e6f2e8158ba4a067bf0f3a6e9a529722
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-02 14:35:26 -0500

    (update) styling to create blue navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import Login from './Login';
+import { RiShip2Fill } from 'react-icons/ri';
 function Navbar() {
   return (
     <>
-      <Link to="/itinerary/12345">Itinerary 12345</Link>
-      <p className="font-mono">This is a nav</p>
+      <nav className=" bg-blue-800">
+        <div className="container py-3 items-center mx-auto flex justify-between">
+          <div className="flex flex-row items-center space-x-3">
+            <h2 className="text-white font-serif text-4xl italic">Voyage</h2>
+            <RiShip2Fill size={'2rem'} color={'white'} />
+          </div>
+          <Login />
+        </div>
+      </nav>
     </>
   );
 }
