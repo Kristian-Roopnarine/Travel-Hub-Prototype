856246e8a0556cfe7abdb3a948493cfb8349b7fb
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-04 19:33:14 -0500

    (add) proptypes

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function Landing({ loggedIn }) {
   if (loggedIn) {
     return <Redirect to="/home" />;
   }
-  return <h2>Landing page</h2>;
+  return (
+    <>
+      <h2>Landing page</h2>
+    </>
+  );
 }
-
+Landing.propTypes = {
+  loggedIn: PropTypes.bool,
+};
 export default Landing;
