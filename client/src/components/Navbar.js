5a3e6b3d013af5e9389742cf42eeddc90a4585ee
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-04 19:32:20 -0500

    (update) background color to match drawkit.io images

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,11 +5,11 @@ import { RiShip2Fill } from 'react-icons/ri';
 function Navbar({ loggedIn }) {
   return (
     <>
-      <nav className="bg-blue-800">
+      <nav className="bg-red-100 ">
         <div className="p-4 items-center mx-auto flex justify-between">
           <div className="flex flex-row items-center space-x-3">
-            <h2 className="text-white font-serif text-4xl italic">Voyage</h2>
-            <RiShip2Fill size={'3rem'} color={'white'} />
+            <h2 className="text-black font-serif text-4xl italic">Voyage</h2>
+            <RiShip2Fill size={'3rem'} color={'black'} />
           </div>
           {loggedIn ? <a>Logout</a> : <Login />}
         </div>
