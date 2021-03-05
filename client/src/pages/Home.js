b6e42bfa790d75c002dad6e1f225b4a209547e23
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-04 21:48:51 -0500

    (add) ListDisplay component that renders Itineraries

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+import ListDisplay from './../components/ListDisplay';
+import Itineraries from './../components/Itineraries';
 const { REACT_APP_SERVER_URL } = process.env;
 
 function Home({ loggedIn, setLoggedIn }) {
@@ -13,13 +15,21 @@ function Home({ loggedIn, setLoggedIn }) {
       setLoggedIn(userStatus);
     })();
   }, []);
-
   if (!loggedIn) {
     return <Redirect to="/" />;
   }
+
   return (
     <>
       <h2>Home page</h2>
+      <div className="flex mx-auto justify-around">
+        <div className="w-1/3">
+          <ListDisplay headerText={'Upcoming Trips'}>
+            <Itineraries />
+          </ListDisplay>
+        </div>
+        <div className="w-1/3">Your travel guides here</div>
+      </div>
     </>
   );
 }
