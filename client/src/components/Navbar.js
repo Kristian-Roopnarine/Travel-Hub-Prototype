8c9d0b6ec3df12339f078af6f629f2b2443f06f9
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-07 15:57:03 -0500

    (update) styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { RiShip2Fill } from 'react-icons/ri';
 function Navbar({ loggedIn }) {
   return (
     <>
-      <nav className="bg-red-100 ">
+      <nav className="bg-red-100" style={{ height: '10vh' }}>
         <div className="p-4 items-center mx-auto flex justify-between">
           <div className="flex flex-row items-center space-x-3">
             <h2 className="text-black font-serif text-4xl italic">Voyage</h2>
