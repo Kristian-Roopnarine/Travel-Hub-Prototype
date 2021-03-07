9c6a7641dafa8906b64b27deb2593ccf7188246b
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2021-03-07 01:21:49 -0500

    (add) styling

--- client/src/pages/Itinerary.js
+++ client/src/pages/Itinerary.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMaps from './../components/GoogleMaps';
+import Members from './../components/Members';
 import { useParams } from 'react-router-dom';
+
+const { REACT_APP_SERVER_URL } = process.env;
 
 function Itinerary() {
   let { id } = useParams();
+  const [itinerary, setItinerary] = useState({});
+  useEffect(() => {
+    (async () => {
+      console.log('Making request');
+      const response = await fetch(`${REACT_APP_SERVER_URL}/itinerary/${id}`, {
+        method: 'GET',
+        credentials: 'include',
+      });
+      const { data } = await response.json();
+      setItinerary(data);
+    })();
+  }, []);
+  useEffect(() => {
+    console.log(itinerary);
+  }, [itinerary]);
   return (
     <>
-      <h2 className="font-mono">The ID of this itinerary is {id}</h2>
-      <p className="font-mono">Your itinerary </p>
-      <GoogleMaps />
+      <div className="flex">
+        <div className="w-1/2 ">
+          {itinerary.title}
+          <div className="flex justify-between">
+            <Members members={[]} />
+          </div>
+        </div>
+        <div className="w-1/2 ">
+          <GoogleMaps />
+        </div>
+      </div>
     </>
   );
 }
